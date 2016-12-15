87bb85f53f90c4580685f100c36bd42194b47985
BenayahTest <manikandan@ben-solutions.com>
2016-12-15 15:22:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,6 +7,10 @@ define('HTTP_CATALOG', 'http://localhost/oc/');
 define('HTTPS_SERVER', 'http://localhost/oc/admin/');
 define('HTTPS_CATALOG', 'http://localhost/oc/');define('HTTP_SERVER', 'http://localhost/oc/admin/');
 define('HTTP_CATALOG', 'http://localhost/oc/');
+define('HTTPS_SERVER', 'http://localhost/oc/admin/');
+define('HTTPS_CATALOG', 'http://localhost/oc/');define('HTTP_SERVER', 'http://localhost/oc/admin/');
+define('HTTP_CATALOG', 'http://localhost/oc/');
+
 
 // HTTPS
 define('HTTPS_SERVER', 'http://localhost/oc/admin/');
